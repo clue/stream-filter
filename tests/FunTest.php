430b1315776deaf5ae3c0f8f178d2be0f673bd02
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-07-01 14:50:07 +0200

    Adding failing test with base64 endcode

--- tests/FunTest.php
+++ tests/FunTest.php
@@ -31,4 +31,16 @@ class FunTest extends PHPUnit_Framework_TestCase
     {
         Filter\fun('unknown');
     }
+
+    public function testFunInBase64()
+    {
+        $encode = Filter\fun('convert.base64-encode', []);
+        $decode = Filter\fun('convert.base64-decode', []);
+
+        $string = 'test';
+        $this->assertEquals(base64_encode($string), $encode($string));
+        $this->assertEquals($string, $decode(base64_encode($string)));
+        $this->assertEquals($string, $decode($encode($string)));
+        $this->assertEquals(null, $encode());
+    }
 }
