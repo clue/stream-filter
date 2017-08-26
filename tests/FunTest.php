0ffcf249f7fced2e73e403058c47c620519c4bc4
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-08-26 09:35:44 +0200

    Removed "array()"

--- tests/FunTest.php
+++ tests/FunTest.php
@@ -44,18 +44,18 @@ class FunTest extends PHPUnit_Framework_TestCase
 
     public function testFunInBase64()
     {
-        $encode = Filter\fun('convert.base64-encode', array());
-        $decode = Filter\fun('convert.base64-decode', array());
+        $encode = Filter\fun('convert.base64-encode');
+        $decode = Filter\fun('convert.base64-decode');
 
         $string = 'test';
         $this->assertEquals(base64_encode($string), $encode($string) . $encode());
         $this->assertEquals($string, $decode(base64_encode($string)));
 
-        $encode = Filter\fun('convert.base64-encode', array());
-        $decode = Filter\fun('convert.base64-decode', array());
+        $encode = Filter\fun('convert.base64-encode');
+        $decode = Filter\fun('convert.base64-decode');
         $this->assertEquals($string, $decode($encode($string) . $encode()));
 
-        $encode = Filter\fun('convert.base64-encode', array());
+        $encode = Filter\fun('convert.base64-encode');
         $this->assertEquals(null, $encode());
     }
 }
